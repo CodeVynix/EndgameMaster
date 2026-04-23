36494e932d32ea4785b48c653da78782ee1314b8
CodeVynix <noyes6926@gmail.com>
2026-04-23 19:04:11 +0530

    many changes

--- Engine/stockfish_wrapper.cpp
+++ Engine/stockfish_wrapper.cpp
@@ -1,7 +1,10 @@
-#include "stockfish_wrapper.h"
-#include <string>
+extern "C" {
 
-// TEMP stub (so build succeeds)
 const char* sf_best_move(const char* fen) {
-    return "e2e4"; // temporary move
+    return "e2e4"; // TEMP TEST
+}
+
+void sf_send_command(const char* cmd) {
+}
+
 }
