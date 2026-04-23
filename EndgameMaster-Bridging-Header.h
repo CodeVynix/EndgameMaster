640ef1eb8394b1eef412655739472ce7e90d99a2
CodeVynix <noyes6926@gmail.com>
2026-04-23 19:41:54 +0530

    hello

--- EndgameMaster-Bridging-Header.h
+++ EndgameMaster-Bridging-Header.h
@@ -1,7 +1,7 @@
 #ifndef EndgameMaster_Bridging_Header_h
 #define EndgameMaster_Bridging_Header_h
 
-const char* sf_best_move(const char* fen);
-void sf_send_command(const char* command);
+void sf_send_command(const char *command);
+const char* sf_best_move(const char *fen);
 
 #endif
